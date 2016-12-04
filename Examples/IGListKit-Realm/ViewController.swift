58d5fd4332acb282639a5705ee756f9dc23f2c55
Adam Fish <af@realm.io>
2016-12-04 15:14:15 -0800

    Initial commit with example project and general folder structure.

---
+++ Examples/IGListKit-Realm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IGListKit-Realm
+//
+//  Created by Adam Fish on 12/4/16.
+//  Copyright © 2016 Adam Fish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
